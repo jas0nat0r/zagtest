9e13eeef5605e21a29b677b3eb64ede2508c8533
Jason Thrasher <jthrasher@jason-thrashers-macbook-pro.local>
2012-12-07 17:29:06 -0800

    changed db.php

--- admin/common/db.php
+++ admin/common/db.php
@@ -5,11 +5,11 @@ $dbPass = "9pmRWEc4;Hn%";
 $dbDatabase = "mediates_elite";
 $web_url = "http://174.121.85.124/~mediates/design_benefits/";
 */
-$dbHost = "localhost";
-$dbUser = "zagtech";
-$dbPass = "P3psiYucky!";
-$dbDatabase = "newsite";
-$web_url = "http://www.zagtech.com/";
+$dbHost = "Data Source=us-cdbr-azure-west-a.cloudapp.net";
+$dbUser = "b075c74642244c";
+$dbPass = "e2e1219d";
+$dbDatabase = "zagtest";
+/*$web_url = "http://www.zagtech.com/";*/
 
 mysql_connect($dbHost,$dbUser,$dbPass);
 mysql_select_db($dbDatabase);
